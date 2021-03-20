a539f2b215f1f02c59fa41d719094bff157f8390
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-03-20 10:54:43 +0300

    refactored request mapping - server

--- src/main/java/com/zhuravishkin/resttemplateserver/controller/Controller.java
+++ src/main/java/com/zhuravishkin/resttemplateserver/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "template")
+@RequestMapping(value = "server")
 public class Controller {
     @GetMapping(value = "/get")
     public ResponseEntity<User> get() {
