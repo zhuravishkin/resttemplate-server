ce6e41360e18d76c258daab193a0c6f14eff3835
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-03-20 11:06:45 +0300

    added request body for POST

--- src/main/java/com/zhuravishkin/resttemplateserver/controller/Controller.java
+++ src/main/java/com/zhuravishkin/resttemplateserver/controller/Controller.java
@@ -1,13 +1,23 @@
 package com.zhuravishkin.resttemplateserver.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhuravishkin.resttemplateserver.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "server")
 public class Controller {
+    private final ObjectMapper objectMapper;
+
+    public Controller(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @GetMapping(value = "/get")
     public ResponseEntity<User> get() {
         User user = new User("Sam", 30);
@@ -15,8 +25,14 @@ public class Controller {
     }
 
     @PostMapping(value = "/post")
-    public ResponseEntity<User> post(@RequestParam String name, @RequestParam Integer age) {
-        User user = new User(name, age);
+    public ResponseEntity<User> post(@RequestParam String surname, @RequestBody String body) {
+        User user = null;
+        try {
+            user = objectMapper.readValue(body, User.class);
+            user.setName(user.getName() + " " + surname);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);
+        }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
--- src/main/java/com/zhuravishkin/resttemplateserver/model/User.java
+++ src/main/java/com/zhuravishkin/resttemplateserver/model/User.java
@@ -2,9 +2,11 @@ package com.zhuravishkin.resttemplateserver.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
